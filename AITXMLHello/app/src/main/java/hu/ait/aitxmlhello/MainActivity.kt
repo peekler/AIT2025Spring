84045b8c9669f56b952d2811a0c37ffc5203b5e3
Péter Ekler <peter.ekler@aut.bme.hu>
2025-02-12 18:29:39 +0100

    Class 03 demos
    - LogCat
    - Activity life cycle demo

--- AITXMLHello/app/src/main/java/hu/ait/aitxmlhello/MainActivity.kt
+++ AITXMLHello/app/src/main/java/hu/ait/aitxmlhello/MainActivity.kt
@@ -2,6 +2,9 @@ package hu.ait.aitxmlhello
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewAnimationUtils
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -11,6 +14,11 @@ import hu.ait.aitxmlhello.databinding.ActivityMainBinding
 import java.util.Date
 
 class MainActivity : AppCompatActivity() {
+
+    companion object
+    {
+        const val TAG_UI = "TAG_UI"
+    }
 
     lateinit var binding: ActivityMainBinding
 
@@ -26,12 +34,22 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
 
         binding.btnShow.setOnClickListener {
+            Log.d(TAG_UI, "Show button pressed")
+
+
             //binding.tvTime.setText("${Date(System.currentTimeMillis()).toString()}")
 
             val numA = binding.etNumA.text.toString().toInt()
-            val numB = binding.etNumA.text.toString().toInt()
+            val numB = binding.etNumB.text.toString().toInt()
+
             val sum = numA + numB
-            binding.tvTime.text = "Sum: $sum"
+            //binding.tvTime.text = getString(R.string.text_sum, sum, "Peter")
+            //<string name="text_sum">Summary: %1$d %2$s</string>
+            binding.tvTime.text = getString(R.string.text_sum, sum.toString())
+
+            revealCard()
+
+            Log.d(TAG_UI, "Calculation ready")
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -40,5 +58,27 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun revealCard() {
+        val x = binding.cardView.getRight()
+        val y = binding.cardView.getBottom()
+
+        val startRadius = 0
+        val endRadius = Math.hypot(binding.cardView.getWidth().toDouble(),
+            binding.cardView.getHeight().toDouble()).toInt()
+
+        val anim = ViewAnimationUtils.createCircularReveal(
+            binding.cardView,
+            x,
+            y,
+            startRadius.toFloat(),
+            endRadius.toFloat()
+        )
+        //anim.setDuration(5000)
+
+        binding.cardView.setVisibility(View.VISIBLE)
+        anim.start()
+    }
+
 
 }
